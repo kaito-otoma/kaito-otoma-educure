1eebac8fe07cf29b0806c17791318bf6ca9d19c6
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-04-03 21:51:02 +0900

    演習課題2-3　修正02

--- 2-3/src/Main6.java
+++ 2-3/src/Main6.java
@@ -2,11 +2,11 @@ public class Main6 {
     public static void main(String[] args) {
 
         int price = 1000 ;
-        System.out.print("更新前の価格:"+price+"円");
+        System.out.println("更新前の価格:"+price+"円");
         
         
-        price = 1200 ;
-        System.out.print("\n更新後の価格:"+price+"円");
+         price = 1200 ;
+         System.out.print("更新後の価格:"+price+"円");
     
     }
 }
