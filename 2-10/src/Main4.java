c58431f1c98e07284d6db5564a947f617ce4c5f5
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-04-09 17:49:52 +0900

    2-10演習課題　修正2

--- 2-10/src/Main4.java
+++ 2-10/src/Main4.java
@@ -1,27 +1,64 @@
 public class Main4 {
     public static void main(String[] args) {
-        int side1 = 1;
-        int side2 = 2;
-        int side3 = 10;
-        double sideA = 1.414;
-        
-        if (side1 + side2 > side3) { //三角形の条件指定 
-        } else if (side1 == side2 && side3 == sideA ) {
-            System.out.println("正三角形");
-        } else if (side1 == side2 && side3 == side1 * sideA){
-            System.out.println("直角二等辺三角形");
-        } else if (side1 == side2 || side1 == side3 || side2 == side3) {
-            System.out.println("二等辺三角形");
-        } else if (side1 * side1 + side2 * side2 == side3 * side3 ||
-                side1 * side1 + side3 * side3 == side2 * side2 || 
-                side2 * side2 + side3 * side3 == side1 * side1){
-            System.out.println("直角三角形");
-        } else if (side1 != side2 && side1 != side3 && side2 != side3 && side1 + side2 > side3) {
-            System.out.println("不等辺三角形");
+        int side1 = 3;
+        int side2 = 4;
+        int side3 = 5;
+       
+        int shortSide = 0;   
+        int middleSide = 0;   
+        int longSide = 0;     
+
+        if (side1 > side2) {
+            if (side1 > side3) {
+                longSide = side1;
+                if (side2 > side3) {
+                    middleSide = side2;
+                    shortSide = side3;
+                } else {
+                    middleSide = side3;
+                    shortSide = side2;
+                }
+            } else {
+                longSide = side3;
+                middleSide = side1;
+                shortSide = side2;
+            }
         } else {
-            System.out.println("三角形ではない");
+            if (side2 > side3) {
+                longSide = side2;
+                if (side1 > side3) {
+                    middleSide = side1;
+                    shortSide = side3;
+                } else {
+                    middleSide = side3;
+                    shortSide = side1;
+                }
+            } else {
+                longSide = side3;
+                middleSide = side2;
+                shortSide = side1;
+            }
         }
-            
+
+        //三角形種類判定
+        if (shortSide + middleSide < longSide) {
+            System.out.println("三角形ではない");
+        } else if (shortSide == middleSide && middleSide == longSide) {
+            System.out.println("正三角形");
+        } else {
+            if (shortSide == middleSide || middleSide == longSide) {
+                if (shortSide * shortSide + middleSide * middleSide == longSide * longSide) {
+                    System.out.println("直角二等辺三角形");
+                } else {
+                    System.out.println("二等辺三角形");
+                }
+            } else if (shortSide * shortSide + middleSide * middleSide == longSide * longSide) {
+                System.out.println("直角三角形");
+            } else {
+                System.out.println("不等辺三角形");
+            }
+        }
     }
 }
+
  
