9f44bc2b5381d589067ccfa7a389db00a5df3bf2
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-04-21 18:09:57 +0900

    2-18演習課題　修正3

--- 2-18/src/Main2.java
+++ 2-18/src/Main2.java
@@ -2,18 +2,19 @@ public class Main2 {
     public static void main(String[] args) {
         int score = 90;  // 学生の得点
 
-        scoreRank(score);
+        String result = scoreRank(score);
+        System.out.println(result);
     }
-    public static void scoreRank(int score){
+    public static String scoreRank(int score){
 
         if (score >= 80) {
-            System.out.println("成績:優");
+            return"成績:優";
         } else if (score >= 70) {
-            System.out.println("成績:良");
+            return"成績:良";
         } else if (score >= 60) {
-            System.out.println("成績:可");
+            return"成績:可";
         } else {
-            System.out.println("成績:不可");
+            return"成績:不可";
         }
     }
 }
