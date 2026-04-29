c19ff6eb8cb7213b04703ba49c96e41825efe3d4
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-04-29 18:47:22 +0900

    Delete 2-21/src/Main4/collect.java

--- 2-21/src/Main4/collect.java
+++
@@ -1,9 +0,0 @@
-package Main4;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class collect {
-    public static List<Integer> ristNumber = new ArrayList<>(Arrays.asList(5, 2, 8, 1, 9));
-}
