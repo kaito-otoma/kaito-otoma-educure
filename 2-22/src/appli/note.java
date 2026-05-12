ada4c5b47bb12031c966b18f0479ae59a51553e9
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-05-12 20:44:04 +0900

    2-22総合演習　修正4

--- 2-22/src/appli/note.java
+++ 2-22/src/appli/note.java
@@ -5,18 +5,18 @@ public class note {
     public static int start(String[] englishWord, String[] japaneseWord, int count) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            if (count >= 20) {
+    while (true) {
+        if (count >= 20) {
             System.out.println("エラー表示: 登録可能な単語数は20個までです");
             break;
-            }
+        }
 
             System.out.print("英単語を入力してください: ");
             englishWord[count] = scanner.nextLine();
             System.out.print("日本語訳を入力してください: ");
             japaneseWord[count] = scanner.nextLine();
             count++;
-        
+            break;
         }
         return count;
     } 
