b460f638d6a12c034faffc43aff1d7c6595fd8d9
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-05-18 11:09:39 +0900

    3-3演習問題　修正3

--- 3-3/src/Main2/Main2.java
+++ 3-3/src/Main2/Main2.java
@@ -4,7 +4,7 @@ public class Main2 {
     public static void main(String[] args) {
         Book bookName = new Book();
 
-        bookName.getTitle("Java入門");
+        bookName.setTitle("Java入門");
         bookName.isLent = false;
 
         System.out.println("「" + bookName.title + "」" + "を登録しました");
