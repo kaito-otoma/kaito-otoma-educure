91fec4ab215e27bc2fcf2dccd64134d9013d8b9c
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-05-29 15:08:46 +0900

    3-8演習課題　修正1

--- 3-8/src/Main1/Main1.java
+++ 3-8/src/Main1/Main1.java
@@ -2,7 +2,7 @@ package Main1;
 
 public class Main1 {
     public static void main(String[] args) {
-        Swod swod = new Swod();
+        Sword swod = new Sword("ブロンズソード", 10);
         System.out.println("武器名:" + swod.name);
         System.out.println("攻撃力:" + swod.power);
     }
---
+++ 3-8/src/Main1/Sword.java
@@ -0,0 +1,8 @@
+package Main1;
+
+public class Sword extends Wepon{
+    public Sword(String  name, int power){
+        this.name = name;
+        this.power = power;
+    }
+}
