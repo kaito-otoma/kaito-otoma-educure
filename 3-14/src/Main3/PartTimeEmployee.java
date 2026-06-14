aed6324251df79ed99a1bd1dcd24650331e0205a
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-06-14 19:20:25 +0900

    3-14　演習課題　修正1

--- 3-14/src/Main3/PartTimeEmployee.java
+++ 3-14/src/Main3/PartTimeEmployee.java
@@ -1,17 +1,18 @@
 package Main3;
 
 public class PartTimeEmployee extends Employee{
+    String role;
     int hourlyWage;
     int hoursWorked;
 
-    public PartTimeEmployee(String name, int hourlyWage, int hoursWorked){
+    public PartTimeEmployee(String role, String name, int hourlyWage, int hoursWorked){
         super(name);
         this.hourlyWage = hourlyWage;
         this.hoursWorked = hoursWorked;
     }
     @Override
     public String getRole(){
-        return"";
+        return role;
     }
     @Override
     public int calculateSalary(){
--- 3-14/src/Main3/RegularEmployee.java
+++ 3-14/src/Main3/RegularEmployee.java
@@ -1,15 +1,16 @@
 package Main3;
 
 public class RegularEmployee extends Employee {
+    String role;
     int baseSalary;
 
-    public RegularEmployee(String name, int baseSalary){
+    public RegularEmployee(String role, String name, int baseSalary){
         super(name);
         this.baseSalary = baseSalary;
     }
     @Override
     public String getRole(){
-        return"";
+        return role;
     }
      @Override
     public int calculateSalary() {
