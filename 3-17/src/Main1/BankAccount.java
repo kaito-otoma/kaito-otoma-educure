0154c6651a3d549cdf079665648bd74f5218b566
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-06-17 10:28:27 +0900

    3-17演習課題　修正2

--- 3-17/src/Main1/BankAccount.java
+++ 3-17/src/Main1/BankAccount.java
@@ -26,11 +26,12 @@ public class BankAccount {
             System.out.println("エラー: 正の整数を入力してください。");
             return;
         }
+        if(amount > balance){
+            System.out.println("引き出しに失敗しました。残高が不足しています");
+            return;
+        }
         balance -= amount;
             System.out.println(amount + "円引き出しました");
             System.out.println("残高:" + balance + "円");
-        if(0 > balance){
-            System.out.println("引き出しに失敗しました。残高が不足しています");
-        }
     }
 }
