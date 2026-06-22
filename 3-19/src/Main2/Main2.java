995fa7e539c244d1cbca2aa277786f24e5f0c817
kaito-otoma <k.otoma-tunagu@kotobuki8.net>
2026-06-22 13:21:16 +0900

    3-19演習課題　修正01

--- 3-19/src/Main2/Main2.java
+++ 3-19/src/Main2/Main2.java
@@ -3,9 +3,11 @@ package Main2;
 public class Main2 {
     public static void main(String[] args) {
     Student student = new Student(1001, "田中太郎");
-    Student student2 = new Student(1002, "佐藤花子");
+    Student student2 = new Student(1001, "田中太郎");
+    Student student3 = new Student(1002, "佐藤花子");
 
     
     System.out.println(student.equals(student2)? "同一の学生です" : "違う学生です");
+    System.out.println(student.equals(student3)? "同一の学生です" : "違う学生です");
     }
 }
--- 3-19/src/Main3/Library.java
+++ 3-19/src/Main3/Library.java
@@ -1,18 +1,15 @@
 package Main3;
 
 public class Library {
-    static int availableBooks;
+    static int availableBooks = 100;
 
-    public Library(){
-        availableBooks = 100;
-    }
-    public void view(){
+    public  void view(){
         System.out.println("貸し出し可能な本:" + availableBooks + "冊");
     }
     public void borrowBook(){
     if(availableBooks <= 0){
-        System.out.println("貸し出し可能な本:" + availableBooks + "冊");
         System.out.println("貸し出し可能な本がありません");
+        return;
     }
         availableBooks--;
         System.out.println("1冊借りました");
@@ -22,5 +19,8 @@ public class Library {
         availableBooks++;
         System.out.println("本を返却しました");
         System.out.println("貸し出し可能な本:" + availableBooks + "冊");
+    }
+    public static int getAvailableBooks() {
+        return availableBooks;
     }
 }
